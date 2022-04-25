2aed93cd748dfbe00d7dfd3bbef27c11b94c0b5c
Chengxu Liu <41517906+ChengxuLiu@users.noreply.github.com>
2022-04-25 15:07:51 +0800

    Update restoration_video_inference.py

--- mmedit/apis/restoration_video_inference.py
+++ mmedit/apis/restoration_video_inference.py
@@ -6,6 +6,34 @@ from mmcv.parallel import collate, scatter
 from mmedit.datasets.pipelines import Compose
 
 
+def init_model(config, checkpoint=None, device='cuda:0'):
+    """Initialize a model from config file.
+    Args:
+        config (str or :obj:`mmcv.Config`): Config file path or the config
+            object.
+        checkpoint (str, optional): Checkpoint path. If left as None, the model
+            will not load any weights.
+        device (str): Which device the model will deploy. Default: 'cuda:0'.
+    Returns:
+        nn.Module: The constructed model.
+    """
+    if isinstance(config, str):
+        config = mmcv.Config.fromfile(config)
+    elif not isinstance(config, mmcv.Config):
+        raise TypeError('config must be a filename or Config object, '
+                        f'but got {type(config)}')
+    config.model.pretrained = None
+    config.test_cfg.metrics = None
+    model = build_model(config.model, test_cfg=config.test_cfg)
+    if checkpoint is not None:
+        checkpoint = load_checkpoint(model, checkpoint)
+
+    model.cfg = config  # save the config in the model for convenience
+    model.to(device)
+    model.eval()
+    return model
+
+
 def pad_sequence(data, window_size):
     padding = window_size // 2
 
